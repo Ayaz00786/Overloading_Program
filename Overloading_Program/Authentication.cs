7fe98f8ec74c42ca1bad74dfe822a5c24586c622
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-05-08 20:27:42 +0530

    Add project files.

---
+++ Overloading_Program/Authentication.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Overloading_Program
+{
+    public class Authentication
+    {
+        public void LoginUser(string email, string password)
+        {
+            if (email == "ayaj123@gmail.com" && password == "12345")
+            {
+                Console.WriteLine("Login Successfully");
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Correct Email and Password !!");
+            }
+        }
+        public void LoginUser(string mobile)
+        {
+            if (!string.IsNullOrEmpty(mobile))
+            {
+                Console.WriteLine("enter your Mobile:");
+                string mob= Console.ReadLine();
+
+                // send otp to the mobile number given by users
+                Console.WriteLine("otp sent to "+mob +" Number");
+               
+            }
+            else
+            {
+                Console.WriteLine("Enter correct number");
+            }
+        }
+    }
+}
---
+++ Overloading_Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Overloading_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Authentication AUTH=new Authentication();
+            string email = "ayaj123@gmail.com";
+            string password = "12347";
+            string mobile= "9340336471";
+
+            AUTH.LoginUser(mobile);
+        }
+    }
+}
